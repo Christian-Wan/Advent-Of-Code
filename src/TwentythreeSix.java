08376db6b4469a5a13b544237a62b1b7874f8c04
Christian Wan <christian.wan295@gmail.com>
2023-12-06 00:14:54 -0500

    Why was it so easy

---
+++ src/TwentythreeSix.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+
+public class TwentythreeSix {
+    public static void main(String[] args) throws FileNotFoundException {
+        int time = 46689866;
+        String part1 = "3581054";
+        String part2 = "18071080";
+        long distance = Long.parseLong(part1 + part2);
+
+
+        int count = 0;
+        for (int x = 0; x < time; x++) {
+            int remain = time - x;
+            if ((long) remain * x > distance) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
