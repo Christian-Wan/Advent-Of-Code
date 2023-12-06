ffb4c8cbfed3b2427fc0f059b5e3bf35df52deaa
Christian Wan <christian.wan295@gmail.com>
2023-12-05 21:21:19 -0500

    reverse it

--- src/TwentythreeFive.java
+++ src/TwentythreeFive.java
@@ -8,23 +8,36 @@ public class TwentythreeFive {
     public static void main(String[] args) throws FileNotFoundException {
         File f = new File("Twentythree/five");
         Scanner s = new Scanner(f);
-        String[] conversionSequence = {"seed", "soil", "fertilizer", "water", "light", "temperature", "humidity"};
         String line = s.nextLine();
         String[] seeds = line.substring(line.indexOf(":") + 2).split(" ");
-        ArrayList<String> realSeeds = new ArrayList<>();
         long smallest = 0;
+        String file = line;
+        while (s.hasNext()) {
+            file += "\n" + s.nextLine();
+        }
+        file += "\n";
+        ArrayList<long[]> seedsToSoil = conversionFinder("seed", file);
+        ArrayList<long[]> soilToFertilizer = conversionFinder("soil",file);
+        ArrayList<long[]> fertilizerToWater = conversionFinder("fertilizer",file);
+        ArrayList<long[]> waterToLight = conversionFinder("water",file);
+        ArrayList<long[]> lightToTemperature = conversionFinder("light",file);
+        ArrayList<long[]> TemperatureToHumidity = conversionFinder("temperature",file);
+        ArrayList<long[]> humidityToLocation = conversionFinder("humidity",file);
+        ArrayList[] conversionSequence = {seedsToSoil, soilToFertilizer, fertilizerToWater, waterToLight, lightToTemperature, TemperatureToHumidity, humidityToLocation};
 
 
-        for (int i = 0; i < seeds.length; i++) {
+        s.close();
+
+        for (int i = 0; i < seeds.length; i += 2) {
+            System.out.println("next" + i);
             for (int x = 0; x < Long.parseLong(seeds[i + 1]); x++) {
-                String before = Long.toString(Long.parseLong(seeds[i]) + x);
-                System.out.println(before);
-                for (String converting : conversionSequence) {
-                    before = conversionFinder(converting, before);
+                long before = Long.parseLong(seeds[i]) + x;
+                for (ArrayList converting : conversionSequence) {
+                    before = findConversion(converting, before);
                 }
-                if (smallest == 0 || smallest > Long.parseLong(before)) {
-                    smallest = Long.parseLong(before);
-                    System.out.println("----");
+                if (smallest == 0 || smallest > before) {
+                    smallest = before;
+                    System.out.println("new:" + before);
 
                 }
             }
@@ -32,36 +45,45 @@ public class TwentythreeFive {
         System.out.println(smallest);
     }
 
-    public static String conversionFinder(String input, String converting) throws FileNotFoundException {
-        File f = new File("Twentythree/five");
-        Scanner s = new Scanner(f);
+
+
+    public static ArrayList<long[]> conversionFinder(String input, String file) {
         int counter = 0;
-        String result = "";
-        while (s.hasNext() && counter != 2) {
-            String line = s.nextLine();
+        ArrayList<long[]> big = new ArrayList<>();
+        while (file.contains("\n") && counter != 2) {
+            String line = file.substring(0, file.indexOf("\n"));
+            file = file.substring(file.indexOf("\n") + 1);
             if (line.contains(input)) {
                 counter++;
             }
         }
-        while (s.hasNext()) {
-            String line = s.nextLine();
+        while (file.contains("\n")) {
+            long[] small = new long[3];
+            String line = file.substring(0, file.indexOf("\n"));
+            file = file.substring(file.indexOf("\n") + 1);
             String[] range = line.split(" ");
-            long conversion;
             try {
-                conversion = Long.parseLong(range[0]);
+                long test = Long.parseLong(range[0]);
             }
             catch (NumberFormatException e) {
                 break;
             }
-            long start = Long.parseLong(range[1]);
-            long distance = Long.parseLong(range[2]);
-            if (Long.parseLong(converting) >= start && Long.parseLong(converting) < start + distance) {
-                result = Long.toString((Long.parseLong(converting) - start) + conversion);
+            for (int i = 0; i < range.length; i++) {
+                    small[i] = Long.parseLong(range[i]);
+            }
+            big.add(small);
+        }
+        return big;
+    }
+
+    public static long findConversion(ArrayList<long[]> type, long input) {
+
+        for (int i = 0; i < type.size(); i++) {
+            if (type.get(i)[1] <= input && input < type.get(i)[1] + type.get(i)[2]) {
+
+                return input - type.get(i)[1] + type.get(i)[0];
             }
         }
-        if (result.equals("")) {
-            result = converting;
-        }
-        return result;
+        return input;
     }
 }
