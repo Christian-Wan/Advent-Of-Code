eafc6533b5d6055f18296df13b151d9202701b36
Christian Wan <christian.wan295@gmail.com>
2024-01-04 14:05:53 -0500

    finish later

---
+++ src/Year15_Day10.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Year15_Day10 {
+    public static void main(String[] args) {
+        String number = "3113322113";
+        String newNumber = "";
+        String foundNumbers = "";
+        int counter = 0;
+        System.out.println(number);
+        for (int i = 0; i < 40; i++) {
+            foundNumbers = "";
+            for (int x = 0; x < number.length(); i++) {
+                if (foundNumbers.contains(number.substring(x, x  + 1))) {
+
+                }
+            }
+        }
+    }
+}
